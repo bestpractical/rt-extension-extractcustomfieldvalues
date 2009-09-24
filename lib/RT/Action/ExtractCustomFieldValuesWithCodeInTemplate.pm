5d05eccb9eb98260adb24f63eb6cd60299cc856d
Shawn M Moore <sartak@bestpractical.com>
2009-09-24 10:31:10 -0400

    Implementation using Text::Template

--- lib/RT/Action/ExtractCustomFieldValuesWithCodeInTemplate.pm
+++ lib/RT/Action/ExtractCustomFieldValuesWithCodeInTemplate.pm
@@ -6,10 +6,13 @@ use base qw(RT::Action::ExtractCustomFieldValues);
 
 sub TemplateContent {
     my $self = shift;
+
     my $content = $self->TemplateObj->Content;
 
+    my $template = Text::Template->new(TYPE => 'STRING', SOURCE => $content);
+    my $new_content = $template->fill_in;
 
-    return $content;
+    return $new_content;
 }
 
 1;
