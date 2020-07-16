121a1cac197a09b02608cf6b81efe029bf0acbc7
sunnavy <sunnavy@bestpractical.com>
2020-07-17 03:03:05 +0800

    Update copyright year to 2020

--- lib/RT/Extension/ExtractCustomFieldValues.pm
+++ lib/RT/Extension/ExtractCustomFieldValues.pm
@@ -220,7 +220,7 @@ or via the web at
 
 =head1 LICENSE AND COPYRIGHT
 
-This software is Copyright (c) 2007-2014 by Best Practical Solutions, LLC.
+This software is Copyright (c) 2007-2020 by Best Practical Solutions, LLC.
 
 This is free software, licensed under:
 
